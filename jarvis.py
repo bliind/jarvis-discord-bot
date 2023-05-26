66ef040b78d45e71f5ccba39e5e1c95153fd0fce
Mike Sweeney <blindx@gmail.com>
2023-05-25 22:14:34 -0400

    Fail other things gracefully

--- jarvis.py
+++ jarvis.py
@@ -59,8 +59,10 @@ async def send_devreply_embed(message, thread_open):
 
     embed.set_thumbnail(url=message.author.display_avatar.url)
     sent = await output_channel.send(embed=embed)
-    await sent.add_reaction(config.upvote_emoji)
-    await sent.add_reaction(config.downvote_emoji)
+    try: await sent.add_reaction(config.upvote_emoji)
+    except: print('Could not add upvote emote')
+    try: await sent.add_reaction(config.downvote_emoji)
+    except: print('Could not add downvote emote')
     try: await sent.publish()
     except: print('Could not Publish message')
 
